d15baf6513e3a3497a6818a7178ef68de5ac7c91
Shiva Kumar <shivakumarkothapally5@gmail.com>
2025-06-25 14:19:47 +0530

    Utility UI

--- react_pro/src/App.js
+++ react_pro/src/App.js
@@ -31,39 +31,6 @@ function App() {
     <>
     <BrowserRouter>
 
-{/* <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow sticky-top">
-  <div class="container-fluid">
-   
-    <a class="navbar-brand fw-bold text-uppercase" href="/">Home</a>
-
-    
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-  
-    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-      <ul class="navbar-nav d-flex flex-row gap-4">
-        <li class="nav-item"><a class="nav-link text-white" href="/assets">Assets</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="/maintenance">Maintenance</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="/workorder">Work Order</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="/technicians">Technicians</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="/assign">Assign</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="/assignments">Assignments</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="/report">Report</a></li>
-      </ul>
-    </div>
-
-    
-    <div class="d-flex">
-      <a href="/login">
-        <button class="btn btn-warning">Login</button>
-      </a>
-    </div>
-  </div>
-</nav> */}
-
 
       <Navbar/>
      <Routes>
@@ -78,10 +45,7 @@ function App() {
         <Route path="/add-maintenance" element={<CreatePlan />} />
         <Route path="/tasks/:planId" element={<Tasklist />} />
         <Route path="/update-maintenance/:planId" element={<UpdateMaintenancePlan />} />
-        {/* <Route path='/update-maintainencePlan/:planId' element={<CreatePlan/>}></Route> */}
-          
-          
-         {/* <Route path='/workorder' element={<ListAssetsComponent/>}></Route> */}
+       
          
          <Route path='/report' element={<Reporting/>}></Route> 
           
